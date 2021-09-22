5bcaaf0916810e3363169247041a457847508d43
Toshiy Do <toshiydo@gmail.com>
2021-09-22 14:17:10 +0500

    Исправление тестого метода

--- test_items.py
+++ test_items.py
@@ -1,7 +1,8 @@
-from selenium import webdriver
-import time
-def test_items(browser):
 
+
+def test_button_addToBasket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    browser.get(link)
     browser.implicitly_wait(5)
     button = browser.find_element_by_css_selector('.btn.btn-add-to-basket')
     check_button = button.get_attribute('type')
