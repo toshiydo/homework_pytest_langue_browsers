f4af9dd450ae8912667503858b28417cddd3a951
Toshiy Do <toshiydo@gmail.com>
2021-09-22 14:13:51 +0500

    Перенос линка в test_items

--- conftest.py
+++ conftest.py
@@ -20,18 +20,15 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     browser = None
-    link = f'http://selenium1py.pythonanywhere.com/{user_language}/catalogue/coders-at-work_207/'
     if browser_name == "chrome":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
-        browser.get(link)
         print("\nstart chrome browser for test..")  
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
-        browser.get(link)
         print("\nstart firefox browser for test..")
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
